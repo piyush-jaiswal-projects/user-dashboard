de7011262f26070ee58fa21818a083bab2de19b5
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-07-23 16:35:59 +0530

    v1.0 - Small Form UI

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -9,9 +9,9 @@ export default function Form() {
       className="absolute z-40 hidden w-[100vw] h-screen bg-gray-900 bg-opacity-60"
     >
       <div
-        className="bg-white z-50 absolute md:relative bottom-5 md:bottom-0 shadow-lg overflow-hidden 
-        max-h-[85vh] md:max-h-0 md:min-h-[98vh] sm:h-[100vh] mx-[2.5%] sm:mx-[15%] md:mx-auto my-[0.5vw] 
-        w-[95%] sm:w-[70%] md:w-[50%] min-[1000px]:w-[40%] min-[1200px]:w-[35%] rounded-3xl"
+        className="bg-white z-50 absolute md:relative bottom-0 md:bottom-0 shadow-lg overflow-hidden 
+        max-h-[85vh] md:max-h-0 md:min-h-[98vh] sm:h-[100vh] mx-[0] sm:mx-[15%] md:mx-auto sm:my-[0.5vw] 
+        w-[100%] sm:w-[70%] md:w-[50%] min-[1000px]:w-[40%] min-[1200px]:w-[35%] rounded-t-3xl sm:rounded-3xl"
       >
         <Header />
         <FormFields />
